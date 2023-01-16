5f988100fac2530c95fc4b56eff70693c8ab77db
Petrachkova <disprove69@gmail.com>
2023-01-16 11:59:39 +0300

    Add Example001

---
+++ Seminar/Seminar001/Example001_New/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+// int a = int.Parse(Console.Readline());
+int b = a*a;
+// System.Console.WriteLine();
+Console.WriteLine("Квадрат числа равен:= " + b);
