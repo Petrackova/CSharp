a4421771059597e042ada6566d77be7f3ba26e81
Petrachkova <disprove69@gmail.com>
2023-01-16 12:06:05 +0300

    Add Example005

---
+++ Seminar/Seminar001/Example005_Number/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if ((number1> 100) && (number1<999))
+{
+    Console.WriteLine(number1%10);
+}
+else {
+   Console.WriteLine("Число не трехзначное"); 
+}
