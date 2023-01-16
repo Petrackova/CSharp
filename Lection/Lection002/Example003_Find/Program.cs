73113e1bfb2e0767a7943b77aac3856c3cabf757
Petrachkova <disprove69@gmail.com>
2023-01-16 14:19:18 +0300

    Add Example003

---
+++ Lection/Lection002/Example003_Find/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] Array = {1432, 77, 3345, 4, 565, 3, 77, 8234};
+int n = Array.Length;
+int find =77;
+int index = 0;
+while (index< n)
+{
+    if (Array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
+
