1d56e333172c56d005cca4bafd9e2e276db8f9f1
Petrachkova <disprove69@gmail.com>
2023-01-16 11:46:09 +0300

    Add Example005

---
+++ Lection/Lection001/Example005_HelloMasha/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("Привет! Это же МАША!");
+}
+else {
+Console.Write("Привет,");
+Console.WriteLine(username);
+}
