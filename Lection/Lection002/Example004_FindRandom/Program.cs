c6dcd417bd79b68cc163630ad2e64dc9daa4d9f5
Petrachkova <disprove69@gmail.com>
2023-01-16 14:42:56 +0300

    Add Example

---
+++ Lection/Lection002/Example004_FindRandom/Program.cs
@@ -0,0 +1,47 @@
+﻿void FindArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+while (index<length) 
+{
+    collection[index]= new Random().Next(1,10);
+    index++;
+}
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index<count)
+    {
+        if(collection[index]==find)
+        {
+        position = index;
+        break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
+FindArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 3);
+Console.WriteLine(pos);
