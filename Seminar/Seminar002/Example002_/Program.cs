9304eb09f2720da26b20092654283c4880bd25d9
Petrachkova <disprove69@gmail.com>
2023-01-17 20:41:55 +0300

    Add Example002

---
+++ Seminar/Seminar002/Example002_/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа]
+int num =  new Random().Next(100,1000);
+Console.WriteLine(num);
+int firssNum = ((num/100)*10)+(num%10);
+Console.WriteLine(firssNum);
+
