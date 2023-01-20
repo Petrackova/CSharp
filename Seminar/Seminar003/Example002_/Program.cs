d9c72c404b873d949a2e64a2158039e3680ab144
Petrachkova <disprove69@gmail.com>
2023-01-20 21:12:10 +0300

    Add Seminar3

---
+++ Seminar/Seminar003/Example002_/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+Random rnd = new Random();
+Console.WriteLine("Введите координаты точки A");
+int XA = rnd.Next(1,11);
+int YA = rnd.Next(1,11);
+Console.WriteLine($"Кородинаты точки A: {XA} {YA}");
+Console.WriteLine("Введите координаты точки B");
+int XB = rnd.Next(1,11);
+int YB = rnd.Next(1,11);
+Console.WriteLine($"Кородинаты точки B: {XB} {YB}");
+double x = Math.Pow((XB - XA), 2);
+double y = Math.Pow((YB - YA), 2);
+double AB = Math.Sqrt(x + y);
+Console.WriteLine($"Расстояние между точками: {AB}");
---
+++ Seminar/Seminar003/Example003_/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+int N = new Random().Next(1,11);
+Console.WriteLine(N);
+Console.WriteLine("Таблица квадратов");
+for (int i = 0; i <= N; i++)
+{
+    double sqrt = Math.Pow((i), 2);
+    Console.WriteLine($"{i} ^2 = {sqrt}");
+}
