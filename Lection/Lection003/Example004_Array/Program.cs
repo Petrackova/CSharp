c8c98b8b04dff9ce155061dfdeb5f1eedc377a99
Petrachkova <disprove69@gmail.com>
2023-01-23 12:11:29 +0300

    Add Example004

---
+++ Lection/Lection003/Example004_Array/Program.cs
@@ -0,0 +1,36 @@
+﻿// Упорядочить массив
+int [] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for ( int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+void Sel(int [] array)
+{
+    for ( int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+
+        for ( int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[min])
+            {
+                min = j; 
+            }
+        }
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    }
+}
+
+PrintArray(arr);
+Sel(arr);
+System.Console.WriteLine();
+PrintArray(arr);
