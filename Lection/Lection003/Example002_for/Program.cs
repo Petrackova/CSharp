33849d190c287a274f099bbba10ea4d85a40e387
Petrachkova <disprove69@gmail.com>
2023-01-23 11:43:15 +0300

    Add Example002

---
+++ Lection/Lection003/Example002_for/Program.cs
@@ -0,0 +1,20 @@
+﻿string Method4(int count, string c)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+System.Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    System.Console.WriteLine();
+}
