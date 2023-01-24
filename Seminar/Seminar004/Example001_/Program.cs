bc025028a24cf3df098c6436355056ade3e805de
Petrachkova <disprove69@gmail.com>
2023-01-24 20:08:06 +0300

    Add Example001

---
+++ Seminar/Seminar004/Example001_/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int A ( int arg1)
+{
+    int result = 0;
+    for (int i = 0; i <= arg1; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+int number1 = new Random().Next(1,10);
+System.Console.WriteLine(number1);
+System.Console.WriteLine(A(number1));
