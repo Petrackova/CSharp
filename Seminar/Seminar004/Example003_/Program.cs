20c5526de5c721076c9a550e239d5bbf5c2eef98
Petrachkova <disprove69@gmail.com>
2023-01-24 20:32:38 +0300

    Add Example003

---
+++ Seminar/Seminar004/Example003_/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int A ( int arg1)
+{
+    int result = 1;
+    for (int i = 1; i <= arg1; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+int number1 = new Random().Next(1,10);
+System.Console.WriteLine(number1);
+System.Console.WriteLine(A(number1));
+
