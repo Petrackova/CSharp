f1958cde2629a93f179affa36694fac320301387
Petrachkova <disprove69@gmail.com>
2023-01-25 11:58:23 +0300

    Add Example002

---
+++ CSharp/DZ005/Example002_S/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] ar = Arr(10);
+Console.WriteLine("Массив:");
+PrintArr(ar);
+Console.WriteLine();
+Console.WriteLine($"Сумма нечетных чисел в массиве:= {SummNumber(ar)}");
+
+
+int[] Arr(int lenght)
+{
+    int [] result = new int [lenght];
+
+    for ( int i = 0; i < lenght; i++) 
+    {
+        result[i] = new Random().Next(100,1000);
+    }
+    return result;
+}
+
+int SummNumber (int[] arr)
+{
+    int summ = 0;
+    for ( int i = 0; i < arr.Length; i++)
+    {
+        if ( (arr[i] % 2) != 0)
+        {
+            summ = arr[i]+summ;
+        }
+    }
+    return summ;
+}
+
+
+void PrintArr(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+}
