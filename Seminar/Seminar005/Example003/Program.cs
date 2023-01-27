2e594d2f008c20dd15653c4768e5b17e17e92702
Petrachkova <disprove69@gmail.com>
2023-01-27 20:58:04 +0300

    Add Example003

---
+++ Seminar/Seminar005/Example003/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+int[] ar = Arr(10);
+Console.WriteLine(a);
+PrintArr(ar);
+Console.WriteLine(FindNum(ar,a));
+
+
+int[] Arr(int lenght)
+{
+    int [] result = new int [lenght];
+
+    for ( int i = 0; i < lenght; i++) 
+    {
+        result[i] = new Random().Next(0,10);
+    }
+    return result;
+}
+
+void PrintArr(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+bool FindNum (int[] arr, int num)
+{
+    for ( int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
