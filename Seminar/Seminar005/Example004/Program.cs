6fadae0f59ef4a60ab37c43217324b7a59f2dd61
Petrachkova <disprove69@gmail.com>
2023-01-27 20:58:18 +0300

    Add Example004

---
+++ Seminar/Seminar005/Example004/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+int[] ar = Arr(123);
+PrintArr(ar);
+Console.WriteLine(FindNum(ar));
+
+int[] Arr(int lenght)
+{
+    int [] result = new int [lenght];
+
+    for ( int i = 0; i < lenght; i++) 
+    {
+        result[i] = new Random().Next(0,1000);
+    }
+    return result;
+}
+
+void PrintArr(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+int FindNum (int[] arr)
+{
+    int a = 0;
+    for ( int i = 0; i < arr.Length; i++) 
+    {
+        if ((arr[i] > 10) && (arr[i] < 99))
+        {
+            a++;
+        }
+    }
+    return a;
+}
