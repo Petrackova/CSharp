c9cc5a283f808abb6fdae5a9d1a6a562840cb2d6
Petrachkova <disprove69@gmail.com>
2023-01-29 15:32:18 +0300

    Add Examplee004

---
+++ Lection/Lection004/Example004/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+double Fib ( int i)
+{
+    if ( i ==1 || i ==2) 
+    {
+        return i;
+    }
+    else
+    {
+       return Fib(i-1)+Fib(i-2);
+    }
+}
+for (int i = 1; i < 50; i++)
+{
+System.Console.WriteLine(Fib(i));
+}
