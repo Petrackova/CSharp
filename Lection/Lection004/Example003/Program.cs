9d0192542f4250b70312fe12d5bd4afd4cbb33e3
Petrachkova <disprove69@gmail.com>
2023-01-29 15:32:35 +0300

    Add Example003

---
+++ Lection/Lection004/Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+double Fact ( int a)
+{
+    if ( a == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return a * Fact(a-1);
+    }
+}
+for ( int i = 1; i < 40; i++)
+{
+System.Console.WriteLine($"{i}! = {Fact(i)}");
+}
