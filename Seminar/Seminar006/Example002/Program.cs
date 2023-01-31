d8421d29070f32e0a496e673d71a26e2ed424eb6
Petrachkova <disprove69@gmail.com>
2023-01-31 20:38:07 +0300

    Add example002

---
+++ Seminar/Seminar006/Example002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольниу с сторонами такой длины.( Теорема о неравенстве треугольника)
+int a1 = new Random().Next(1,100);
+int b1 = new Random().Next(1,100);
+int c1 = new Random().Next(1,100);
+Console.WriteLine($"Длина стороны а1:= {a1}");
+Console.WriteLine($"Длина стороны b1:= {b1}");
+Console.WriteLine($"Длина стороны c1:= {c1}");
+Console.WriteLine($"{FindSumm(a1,b1,c1)}");
+  
+bool FindSumm (int a1, int b1, int c1)
+{
+return ((a1< (b1+c1))&&( b1<(a1+c1))&&( c1<(b1+a1)));
+}
