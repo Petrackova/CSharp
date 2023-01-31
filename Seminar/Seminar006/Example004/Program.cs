0f63c22fbe23e1fa25ac31a635cb70e4a66a9934
Petrachkova <disprove69@gmail.com>
2023-01-31 21:21:28 +0300

    Add example004

---
+++ Seminar/Seminar006/Example004/Program.cs
@@ -0,0 +1,23 @@
+﻿// Фибаначи 0 1 1 2 3 5 8 
+
+Print(9);
+
+void Print(int a)
+{
+    int firstNum = 0;
+    int secondNum = 1;
+    if ( a == 1)
+    {
+    System.Console.WriteLine(firstNum);
+    return;
+    }
+    System.Console.WriteLine(firstNum);
+    System.Console.WriteLine(secondNum);
+     for ( int i = 3; i<=a; i++)
+     {
+         int s = firstNum + secondNum;
+             System.Console.WriteLine(s);
+         firstNum = secondNum;
+         secondNum = s;
+     }
+}
