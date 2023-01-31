f24e899c251f1c10df7923837d308a16711cbae3
Petrachkova <disprove69@gmail.com>
2023-01-31 21:21:54 +0300

    Add Example005

---
+++ Seminar/Seminar006/Example005/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу , которая будет создавать копию заданного массива с помощью поэлементарного копирования
+int[] ar = Arr(10);
+PrintArr(ar);
+System.Console.WriteLine(" ");
+int [] arr = Array(ar);
+PrintArr(arr);
+
+
+int[] Arr(int lenght)
+{
+    int [] result = new int [lenght];
+
+    for ( int i = 0; i < lenght; i++) 
+    {
+        result[i] = new Random().Next(0,10);
+    }
+    return result;
+}
+
+void PrintArr(int[] arr)
+{
+    foreach(int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+int[] Array ( int[]arr)
+{
+    int [] result =new int [arr.Length] ;
+   for ( int i = 0; i < arr.Length; i++) 
+    {
+        result[i] = ar[i];
+    }
+    return result;
+}
