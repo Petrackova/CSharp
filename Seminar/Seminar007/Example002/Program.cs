0b3e7568af98d2bc27683d9ec0fb5583294f7032
Petrachkova <disprove69@gmail.com>
2023-02-03 20:18:09 +0300

    Add Example002

---
+++ Seminar/Seminar007/Example002/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+//m = 3, n = 4.
+//0 1 2 3
+//1 2 3 4
+//2 3 4 5
+Print2dArray(Generator2dArray(3,4));
+
+int[,] Generator2dArray( int a, int b)
+{
+    int[,] result = new int [a,b];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j< result.GetLength(1); j++)
+        {
+            result[i,j] = i+j;
+        }
+    }
+    return result;
+}
+
+void Print2dArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j< arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i,j]} ");
+        }
+        System.Console.WriteLine(" ");
+    }
+}
